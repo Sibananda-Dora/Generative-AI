080886a2299703655b4fd86b991bf3499e4810db
Sibananda <sibanandadora443@gmail.com>
2025-10-05 18:33:21 +0530

    final commit for Runnables for now

---
+++ GenAI(CampusX)/Runnables/runnable_branch.py
@@ -0,0 +1,29 @@
+from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain_core.prompts import PromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+from langchain.schema.runnable import RunnableSequence,RunnableParallel,RunnableBranch,RunnablePassthrough
+
+model=ChatGoogleGenerativeAI(
+    model="gemini-2.5-flash-lite",
+    api_key="AIzaSyCVDThxo56lOp9fT9JfcrjWhU-1Opkg1kc"
+)
+parser=StrOutputParser
+
+prompt1=PromptTemplate(
+    template='Write a detailed report on the following topic.\n {topic}',
+    input_variables=['topic']
+)
+prompt2=PromptTemplate(
+    template='Summarize the following text.\n {text}',
+    input_variables=['text']
+)
+
+generate_chain=RunnableSequence(prompt1,model,parser)
+
+branch_chain=RunnableBranch(
+    (lambda x : len(x.split())>500,RunnableSequence(prompt2,model,parser)),
+    RunnablePassthrough()
+)
+final_chain=RunnableSequence(generate_chain,branch_chain)
+
+print(final_chain.invoke({'topic':'elon vs trump'}))

510d336a302854cbe6aff3516953ff73cfff6fba
Sibananda <sibanandadora443@gmail.com>
2025-10-13 08:57:27 +0530

    first commit for streamlit.

---
+++ Python_Basics/Streamlit/01_streamlit.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+st.title("Language Picker")
+# st.subheader("Good Morning !")
+st.text("Choose a Programming Language from the Dropdown Menu.")
+st.write("Choose : ")
+x=st.selectbox(" ",['','Python','C','C++','js'])
+st.write(f'Nice!! You have selected {x}.')
+
+st.success("You are good to go !")
